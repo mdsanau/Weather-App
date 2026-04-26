6500b8c60150b5c2d9faa5d4800662b56cf2c1b9
md sanaullah <mdsanaullah129@gmail.com>
2026-04-26 20:22:23 +0530

    weather App-Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'weather-app';
+  city = '';
+  weather: any = null;
+  error = '';
+  apiKey = '0040f0c12eedb03900eaa933f655932d'; 
+
+  constructor(private http: HttpClient) {}
+
+  getWeather() {
+    this.error = '';
+    this.weather = null;
+   const url = `https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.apiKey}&units=metric`;
+    
+    this.http.get(url).subscribe({
+      next: (data) => this.weather = data,
+      error: () => this.error = 'Sheher nahi mila! Dobara check karo.'
+    });
+  }
 }
